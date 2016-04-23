2f4ebfc94708df58314957532a1aaf76a24f7224
notfan <notfan@hotmail.com>
2016-04-23 15:31:23 +0800

    test

--- MyApplication/app/src/main/java/com/example/xiwen/myapplication/TabDemoActivity.java
+++ MyApplication/app/src/main/java/com/example/xiwen/myapplication/TabDemoActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TabHost;
 
 public class TabDemoActivity extends AppCompatActivity {
 
@@ -17,6 +18,14 @@ public class TabDemoActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("tab1")
+                .setContent(R.id.view1));
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("tab2")
+                .setContent(R.id.view2));
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("tab3")
+                .setContent(R.id.view3));
     }
 
 }
