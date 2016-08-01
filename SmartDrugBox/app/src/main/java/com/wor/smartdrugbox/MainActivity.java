332bb2f65784e87936a51f367c3eb962273bced1
xiwen <notfan@hotmail.com>
2016-08-01 17:26:05 +0800

    init

--- SmartDrugBox/app/src/main/java/com/wor/smartdrugbox/MainActivity.java
+++ SmartDrugBox/app/src/main/java/com/wor/smartdrugbox/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_avatar);
+        toolbar.setTitle(R.string.nick_name);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
