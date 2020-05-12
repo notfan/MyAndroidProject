af0728f70afef2808722823915ac628273f9489d
notfan <notfan@hotmail.com>
2020-05-12 16:25:04 +0800

    dbhelper更改为外部类

--- SmartDrugBox/app/src/main/java/com/wor/smartdrugbox/db/DbAdapter.java
+++ SmartDrugBox/app/src/main/java/com/wor/smartdrugbox/db/DbAdapter.java
@@ -11,29 +11,4 @@ public class DbAdapter {
     private static final String DATABASE_NAME = "SmartDrugBox.db";
     private static final int DATABASE_VERSION = 1;
 
-    public class DatabaseHelper extends SQLiteOpenHelper {
-        public DatabaseHelper(Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            String tracks_sql = "CREATE TABLE cell_alarm ("
-                    + "_id INTEGER primary key autoincrement, "
-                    + "position integer, "
-                    + "start_date date ,"
-                    + "alarm_time time ,"
-                    + "user_id integer"
-                    + ");";
-            Log.i(TAG, tracks_sql);
-            db.execSQL(tracks_sql);
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS cell_alarm;");
-            onCreate(db);
-        }
-
-    }
 }
